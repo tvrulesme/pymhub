6988335f1e576e7764dc9407988c3af534c3d096
John Williamson <jowilliamson@deloitte.co.uk>
2019-05-11 09:42:33 +0100

    update tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,66 +1,50 @@
-import threading
-import os
-import pty
-import socket
+import unittest
 
+from time import sleep
 
-def create_dummy_port(responses):
-    def listener(port):
-        # continuously listen to commands on the master device
-        while 1:
-            res = b''
-            while not res.endswith(b"\r"):
-                # keep reading one byte at a time until we have a full line
-                res += os.read(port, 1)
-            print("command: %s" % res)
+from pymhub import (get_mhub)
+import asyncio
 
-            # write back the response
-            if res in responses:
-                resp = responses[res]
-                del responses[res]
-                os.write(port, resp)
-    
-    master, slave = pty.openpty()
-    thread = threading.Thread(target=listener, args=[master], daemon=True)
-    thread.start()
-    return os.ttyname(slave)
+class TestBlackbird(unittest.TestCase):
+    def setUp(self):
+        self.responses = {}
+        self.mhub = get_mhub('http://192.168.0.44')
 
-def create_dummy_socket(responses):
-    HOST = '127.0.0.1'
-    PORT = 4001
-
-    
-
-    def listener():
-
-        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        conn.bind((HOST, PORT))
-        conn.listen(10)
-
-        while 1:
-            conn, addr = s.accept()
+    def test_zone_status(self):
+        #power_status
+        status = self.mhub.power_status();
+        self.assertIsNotNone(status, 'Status is None')
         
-        conn.send('Please Input Your Command :\r')
-
-        while True:
-            # Receive data
-            res = conn.recv(1024)
-            print("command: %s" % res)
-
-            # write back the response
-            if res in responses:
-                resp = responses[res]
-                del responses[res]
-                conn.sendall(resp)
-
-            if not res:
-                break
-
-    #while 1:
+        #set_power
+        self.mhub.set_power(not status)
+        newstatus = self.mhub.power_status();
+        self.assertNotEqual(status, newstatus, 'Status Unchanged')
         
-    #    start_new_thread(listener ,(conn,))
+        
+        
+        #zone_status
+        zoneinput = int(self.mhub.zone_status(1));
+        self.assertGreaterEqual(zoneinput, 1, 'Bad input found')
+        switchInput = zoneinput - 1
+        if switchInput < 1:
+            switchInput = zoneinput + 1
+            
+        self.mhub.set_zone_source(1,switchInput)
+        sleep(2)
+        
+        #set_zone_source
+        changed_zone = int(self.mhub.zone_status(1));
+        
+        self.assertNotEqual(zoneinput, changed_zone, 'Status Unchanged')
+        
+        self.mhub.set_zone_source(1,zoneinput)
+        
+        self.mhub.set_power(True)
+        sleep(2)
+        self.mhub.set_all_zone_source(2)
+            
+        print(self.mhub.zone_status(1))
 
-    thread = threading.Thread(target=listener, daemon=True)
-    thread.start()
 
-    return HOST
+if __name__ == '__main__':
+   unittest.main()
